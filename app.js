417209879a5eca2440ec6e480b56a513706bf900
Harrison Bonvissuto <admin@ragnaroklab.io>
2022-12-15 00:29:19 -0500

    added event listeners for links, commented js

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
-// function imageToggle() {
-//     let displayImage = document.getElementById('bwTarot')
-//     if (displayImage.src.match('/images/bw_tarot.png')) {
-//         displayImage.src = '/images/color_tarot.png'
-//     } else {
-//         displayImage.src = '/images/bw_tarot.png'
-//     }
-// }
-
-// function cardFlip() {
-//     let shownCard = document.getElementById('bwTarot')
-//     if (shownCard.src.match('/images/bw_tarot.png')) {
-//         shownCard.src = '/images/bw_frame.png'
-
-//     } else if (shownCard.src.match('/images/bw_frame.png'))
-//         shownCard.src = '/images/bw_tarot.png'
-// }
-
-
 const card = document.querySelector(".card");
+const body = document.querySelector('body');
+const cardFace = document.querySelector('.cardFace')
+const twitch = document.getElementById('twitch')
+const youtube = document.getElementById('youtube')
+const twitter = document.getElementById('twitter')
 
+// Add a click event listener to the card element
 card.addEventListener("click", function (e) {
     card.classList.toggle('is-flipped');
 });
+// Add a click event listener to the twitch element
+twitch.addEventListener("click", function (e) {
+    window.open("https://www.twitch.tv", "_blank");
+    card.classList.remove('is-flipped');
+})
+// Add a click event listener to the youtube element
+youtube.addEventListener("click", function (e) {
+    window.open("https://www.youtube.com", "_blank");
+    card.classList.remove('is-flipped');
+})
+// Add a click event listener to the twitter element
+twitter.addEventListener("click", function (e) {
+    window.open("https://www.twitter.com", "_blank");
+    card.classList.remove('is-flipped');
+})
+
+
+// Add a mouseover event listener to the cardFace element
+cardFace.addEventListener("mouseover", function (e) {
+    body.classList.toggle('bg-black')
+});
+// Add a mouseout event listener to the cardFace element
+cardFace.addEventListener("mouseout", function (e) {
+    body.classList.toggle('bg-white')
+});
+
